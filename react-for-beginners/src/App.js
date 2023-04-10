7963894a37f17b92007c3137bc67c23f731b5066
chmh0805 <chmh0805@gmail.com>
2023-04-10 23:30:36 +0900

    learn: Deps what is second parameter of useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,13 +4,26 @@ import { useEffect, useState } from "react";
 
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState("");
+
 	const onClick = () => setCounter((prev) => prev + 1);
+	const onChange = (e) => setKeyword(e.target.value);
+
 	console.log("I run all the time");
 	useEffect(() => {
 		console.log("I run only once.");
 	}, []);
+	useEffect(() => {
+		if (keyword) console.log("SEARCH FOR", keyword);
+	}, [keyword]);
 	return (
 		<div>
+			<input
+				onChange={onChange}
+				type="text"
+				placeholder="Search..."
+				value={keyword}
+			/>
 			<h1 className={styles.title}>{counter}</h1>
 			<Button onclick={onClick} text={"Continue"} />
 		</div>
