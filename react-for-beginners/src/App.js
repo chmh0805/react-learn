5849012667f415b1093fa9a7cba86e0d51f45696
chmh0805 <chmh0805@gmail.com>
2023-04-11 00:16:40 +0900

    learn: useEffect-cleanup function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,31 +1,21 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
+const Hello = () => {
+	useEffect(() => {
+		console.log("Created! :)");
+		return () => console.log("Destroyed! ;("); // cleanUp Function
+	});
+
+	return <h1>Hello</h1>;
+};
+
 function App() {
-	const [counter, setCounter] = useState(0);
-	const [keyword, setKeyword] = useState("");
-
-	const onClick = () => setCounter((prev) => prev + 1);
-	const onChange = (e) => setKeyword(e.target.value);
-
-	console.log("I run all the time");
-	useEffect(() => {
-		console.log("I run only once.");
-	}, []);
-	useEffect(() => {
-		if (keyword) console.log("SEARCH FOR", keyword);
-	}, [keyword]);
+	const [showing, setShowing] = useState(false);
+	const onClick = () => setShowing((prev) => !prev);
 	return (
 		<div>
-			<input
-				onChange={onChange}
-				type="text"
-				placeholder="Search..."
-				value={keyword}
-			/>
-			<h1 className={styles.title}>{counter}</h1>
-			<Button onclick={onClick} text={"Continue"} />
+			{showing ? <Hello /> : null}
+			<button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
 		</div>
 	);
 }
